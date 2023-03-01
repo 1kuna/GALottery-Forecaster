a20e2ae2bbdfb8a8970e6ddf0908b47a06cc182e
asikuna <zcanepa19@gmail.com>
2023-03-01 16:41:34 -0500

    tsf-beta7

--- GAL-TSF.py
+++ GAL-TSF.py
@@ -7,8 +7,11 @@ import tensorflow as tf
 import shutil
 from sklearn.model_selection import train_test_split
 import numpy as np
+import pickle
+import wmi
 import sys
 
+# Set TensorFlow log level to error
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # Initialize current time
@@ -43,18 +46,37 @@ optimizers = ['adam', 'sgd', 'rmsprop', 'adagrad', 'adadelta', 'adamax', 'nadam'
 regularizers = [None, 'l1', 'l2']
 scalers = {'robust': sk.RobustScaler(), 'minmax': sk.MinMaxScaler(), 'standard': sk.StandardScaler()}
 
-# Iterate over each tuner, optimizer, regularization technique
-for tuner in tuners:
-    for optimizer in optimizers:
-        for regularizer in regularizers:
-            for name, scaler in scalers.items():
+# Keep track of the progress within the for loop and resume from the last run if the program is interrupted
+current_tuner_index = 0
+current_optimizer_index = 0
+current_regularizer_index = 0
+current_scaler_index = 0
 
-                # Specify model directory based on tuner, optimizer, regularization technique, and scaler
+# Load the loop state if the program is interrupted
+with open(get_file_path("loop_state.pickle"), "rb") as f:
+    state = pickle.load(f)
+    current_tuner_index, current_optimizer_index, current_regularizer_index, current_scaler_index = state
+    print(f"Resuming from: (tuner: {current_tuner_index}, optimizer: {current_optimizer_index}, regularizer: {current_regularizer_index}, and scaler: {current_scaler_index})")
+
+# Iterate over each tuner, optimizer, regularization technique, and scaler
+for tuner in tuners[current_tuner_index:]:
+    for optimizer in optimizers[current_optimizer_index:]:
+        for regularizer in regularizers[current_regularizer_index:]:
+            for name, scaler in list(scalers.items())[current_scaler_index:]:
+
+                # Save the loop state if the program is interrupted
+                with open(get_file_path("loop_state.pickle"), "wb") as f:
+                    pickle.dump((current_tuner_index, current_optimizer_index, current_regularizer_index, current_scaler_index), f)
+
+                # Print current tuner, optimizer, regularization technique, and scaler
+                print(f"Current tuner: {tuner}")
+                print(f"Current optimizer: {optimizer}")
+                print(f"Current regularizer: {regularizer}")
+                print(f"Current scaler: {name}")
+
+                # Specify model directory and project name based on tuner, optimizer, regularization technique, and scaler
                 model_dir = get_file_path("models beta6")
-                os.makedirs(model_dir, exist_ok=True)
-                
-                # Set the project name
-                project_name = f"{tuner}_{optimizer}_{regularizer}_{name}_{currentTime}"
+                project_name = f"{tuner}_{optimizer}_{regularizer}_{name}_{currentTime}"            
 
                 # Find the previous run
                 latest_model_path = os.listdir(get_file_path("models beta6"))
@@ -77,20 +99,15 @@ for tuner in tuners:
                     )
                 else:
                     # Specify TensorBoard directory based on tuner, optimizer, regularization technique, and scaler
-                    tensorboard_dir = os.makedirs(os.path.join(get_file_path("tensorboard beta6"), project_name))
-                    old_tb_dir = os.makedirs(os.path.join(get_file_path("old tb beta6"), project_name))
+                    tensorboard_dir = os.path.join(get_file_path("tensorboard beta6"), project_name)
+                    old_tb_dir = os.path.join(get_file_path("old tb beta6"), project_name)
+
 
                 # Define callbacks
                 tensorboard_callback = tf.keras.callbacks.TensorBoard(
                     log_dir=tensorboard_dir, histogram_freq=50, 
                     write_graph=True, write_images=True, update_freq='batch', 
-                    profile_batch=1, write_steps_per_second=False
-                )
-                checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
-                    filepath=get_file_path("checkpoints beta6",
-                    filename=(f"{name} {currentTime} checkpoint")), 
-                    save_freq=44700, verbose=1,
-                    save_best_only=True
+                    write_steps_per_second=False
                 )
                 stopping_callback = tf.keras.callbacks.EarlyStopping(
                     monitor="val_loss",
@@ -98,20 +115,25 @@ for tuner in tuners:
                 )
                 
                 # Define callbacks list
-                callbacks = [checkpoint_callback, tensorboard_callback, stopping_callback]
+                callbacks = [tensorboard_callback, stopping_callback]
 
                 # # Write a batch file to initialize the conda tf environment and TensorBoard in the same cmd window with a sleep after the environment is initialized
+                # c = wmi.WMI()
+                # # Write a batch file to initialize the conda tf environment and TensorBoard in the same cmd window with a sleep after the environment is initialized
                 # with open(get_file_path("start_tensorboard.bat"), "w") as f:
-                #     f.write(f"tasklist | find \"chrome.exe\" && taskkill /im chrome.exe /f\n")
                 #     f.write(f"conda activate tf\n")
                 #     f.write(f"sleep 3\n")
                 #     f.write(f"tensorboard --logdir='{tensorboard_dir}'\n")
                 #     f.write(f"sleep 3\n")
-                #     f.write(f"tasklist | find \"chrome.exe\" || start chrome http://localhost:6006/?darkMode=true#hparams\n")
+                #     # Start Chrome if it's not already running
+                #     processes = c.Win32_Process(name='chrome.exe')
+                #     if len(processes) == 0:
+                #         f.write(f'start "" "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe" http://localhost:6006/?darkMode=true#hparams\n')
                 #     f.write(f"sleep 3\n")
 
                 # # Run the batch file
-                # os.system(get_file_path("start_tensorboard.bat"))
+                # batch_file = get_file_path("start_tensorboard.bat").replace("\\", "\\\\")
+                # os.system('"' + batch_file + '"')
 
                 # Initialize the model
                 def run_model():
@@ -125,7 +147,6 @@ for tuner in tuners:
                         overwrite=False,
                         objective='val_loss'
                     )
-
                     return clf
 
                 # Load model checkpoint if it exists, otherwise initialize new model
@@ -148,16 +169,16 @@ for tuner in tuners:
                 x_train_scaled = scaler.fit_transform(x_train)
                 x_val_scaled = scaler.transform(x_val)
                 x_test_scaled = scaler.transform(x_test)
-                
-                # Fit the model
+
+                # Train the AutoKeras model
                 clf.fit(x_train_scaled, y_train, validation_data=(x_val_scaled, y_val), epochs=None, shuffle=False, callbacks=callbacks)
-                
+
                 # Evaluate the model but if there is an error, clear the session and try again
                 try:
                     print("Evaluating model...")
                     predictions = clf.predict(x_test_scaled)
                     error = np.mean((np.abs(y_test - predictions) / np.abs(predictions)) * 100)
-                    print(f"Percentage error: {np.mean((np.abs(y_test - predictions) / np.abs(predictions)) * 100)}")
+                    print(f"Percentage error: {error:.2f}")
                 except:
                     print("Error evaluating model, clearing session and trying again...")
                     tf.keras.backend.clear_session()
@@ -165,22 +186,35 @@ for tuner in tuners:
                     print("Evaluating model...")
                     predictions = clf.predict(x_test_scaled)
                     error = np.mean((np.abs(y_test - predictions) / np.abs(predictions)) * 100)
-                    print(f"Percentage error: {np.mean((np.abs(y_test - predictions) / np.abs(predictions)) * 100)}")
+                    print(f"Percentage error: {error:.2f}")
 
                 # Evaluate the trained model on the test set and print the results to a text file                
                 with open(get_file_path("results beta6", filename="results.txt"), "a") as f:
                     f.write(f"{model_dir}, Percentage Error: {error}\n\n")
 
-                # Put the name of the lowest error model at the top of the text file
+                # Write the model name and error result to a text file and sort the file by error
+                with open(get_file_path("results beta6", filename="results.txt"), "a") as f:
+                    f.write(f"{model_dir}, Percentage Error: {error}\n\n")
                 with open(get_file_path("results beta6", filename="results.txt"), "r") as f:
                     lines = f.readlines()
                 with open(get_file_path("results beta6", filename="results.txt"), "w") as f:
-                    lines.sort(key=lambda x: str(x.split(" ")[-1]))
-                    f.writelines(lines)
+                    if len(lines) > 3:
+                        for line in sorted(lines, key=lambda x: float(x.split(": ")[1]) if len(x.split(": ")) > 1 else 0, reverse=True):
+                            f.write(line)
                 
                 # Move the model to the "finished models" folder and move the TensorBoard model folder to "old tb files" folder if it exists
                 shutil.move(get_file_path(model_dir, filename=project_name), get_file_path("finished models beta6"))
                 if os.path.exists(tensorboard_dir):
                     shutil.move(tensorboard_dir, old_tb_dir)
-                
-                sys.exit()
+
+                # Clear the session
+                tf.keras.backend.clear_session()
+
+                # Increment current scaler index
+                current_scaler_index += 1
+            # Increment current regularizer index
+            current_regularizer_index += 1
+        # Increment current optimizer index
+        current_optimizer_index += 1
+    # Increment current tuner index
+    current_tuner_index += 1
