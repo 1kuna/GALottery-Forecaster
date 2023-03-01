5ff5e7ee39c3f6e3596f9b358dc7ea4f6479ee93
asikuna <zcanepa19@gmail.com>
2023-03-01 16:40:57 -0500

    tsf-beta5

--- GAL-TSF.py
+++ GAL-TSF.py
@@ -7,14 +7,22 @@ import tensorflow as tf
 import shutil
 from sklearn.model_selection import train_test_split
 
-# Get the absolute path of the directory containing the script
-base_path = os.path.dirname(os.path.abspath(__file__))
+# Name current scaling method
+current = 'robust'
 
-# Construct the full file path to the combined csv subfolder, then get the file
-file_path = os.path.join(base_path, "fullcomb", "combined.parquet").replace("/", "\\")
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# Define function to get full file path
+def get_file_path(*subdirs, filename=None):
+    base_path = os.path.dirname(os.path.abspath(__file__))
+    full_path = os.path.join(base_path, *subdirs)
+    if filename is not None:
+        full_path = os.path.join(full_path, filename)
+    full_path = full_path.replace("/", "\\")
+    return full_path
 
 # Read in the paraquet file
-data = pd.read_parquet(file_path)
+data = pd.read_parquet(get_file_path("fullcomb", filename="short.parquet"))
 
 # Define the target column
 target_col = 'WINNING NUMBERS'
@@ -24,8 +32,8 @@ x = data.drop([target_col], axis=1).values
 y = data[target_col].values
 
 # Split the data into training, validation, and test sets (70% for training, 15% for validation, and 15% for testing)
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.15, shuffle=False)
-x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.15, shuffle=False)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.10, shuffle=False)
+x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.10, shuffle=False)
 
 # Initialize scalers
 scalers = {
@@ -44,59 +52,61 @@ for name, scaler in scalers.items():
     x_val_scaled[name] = scaler.transform(x_val)
     x_test_scaled[name] = scaler.transform(x_test)
 
-# Name current scaling method
-current = 'minmax'
-
 # Initialize model project name by date
 f = '%m-%d-%Y %H-%M-%S'
 nw = datetime.datetime.now()
 currentTime = nw.strftime(f)
 
-# Construct the full folder path to the "models" subfolder
-model_save = os.path.join(base_path, "models").replace("/", "\\")
+# Define directories and create if necessary
+model_dir = get_file_path("models")
+os.makedirs(model_dir, exist_ok=True)
 
-csv_save = os.path.join(base_path, "sheets", currentTime).replace("/", "\\")
-os.makedirs(csv_save)
+sheet_dir = get_file_path("sheets", currentTime)
+os.makedirs(sheet_dir, exist_ok=True)
 
 # Save each dataframe into a csv for later predictions
 sets = [("train_scaled", x_train_scaled), ("val_scaled", x_val_scaled), ("test_scaled", x_test_scaled)]
 
 for set_name, set_data in sets:
     set_df = pd.DataFrame(set_data[current])
-    set_df.to_csv(f"{csv_save}\{set_name}.csv", index=False)
+    set_df.to_csv(get_file_path(sheet_dir, filename=f"{set_name}.csv"), index=False, header=True)
 
 # Initialize the AutoKeras model
-clf = ak.TimeseriesForecaster(overwrite=False, max_trials=2500, lookback=21, project_name=(f"beta4 {current} {currentTime}"), directory=model_save)
+clf = ak.TimeseriesForecaster(
+    overwrite=False, max_trials=2, lookback=21, 
+    project_name=(f"beta5 {current} {currentTime}"), 
+    directory=model_dir
+)
 
-# Remove previous tensorboard logs if there are more than 15 folders
-tensorboard_dir = f"{base_path}\\tensorboard"
-tensorboard_subdirs = [d for d in os.listdir(tensorboard_dir) if os.path.isdir(os.path.join(tensorboard_dir, d))]
-if len(tensorboard_subdirs) > 15:
-    oldest_subdir = sorted(tensorboard_subdirs)[0]
-    shutil.rmtree(os.path.join(tensorboard_dir, oldest_subdir))
+# # Remove previous tensorboard logs if they exist
+# tensorboard_dir = get_file_path("tensorboard")
+# tensorboard_subdirs = [d for d in os.listdir(tensorboard_dir) if os.path.isdir(os.path.join(tensorboard_dir, d))]
+# for subdir in tensorboard_subdirs:
+#     shutil.rmtree(os.path.join(tensorboard_dir, subdir))
+
+# Open Tensorboard in browser
+# os.system(f"start cmd /k tensorboard --logdir={tensorboard_dir}")
 
 # Define callbacks
 tensorboard_callback = tf.keras.callbacks.TensorBoard(
-    log_dir=f"{base_path}\\tensorboard", 
-    histogram_freq=30, 
-    write_graph=True,
-    write_images=True, 
-    update_freq='batch', 
-    profile_batch=2
+    log_dir=get_file_path("tensorboard"), histogram_freq=50, 
+    write_graph=True, write_images=True, update_freq='batch', 
+    profile_batch=1, write_steps_per_second=False
 )
-
 checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
-    filepath=f"{base_path}\\checkpoints\\{current} {currentTime}", 
-    save_freq=1
+    filepath=get_file_path("checkpoints",
+    filename=(f"{current} {currentTime} checkpoint")), 
+    save_freq=89400, verbose=1
+)
+stopping_callback = tf.keras.callbacks.EarlyStopping(
+    monitor="val_loss",
+    patience=50,
 )
 
-callbacks = [tensorboard_callback, checkpoint_callback]
+callbacks = [checkpoint_callback, tensorboard_callback, stopping_callback]
 
-# Fit the model to the training data
-clf.fit(x_train_scaled[current], y_train, epochs=None, validation_data=(x_val_scaled[current], y_val), callbacks=callbacks)
-
+# Fit, summarize, evaluate, and export the model
+clf.fit(x_train_scaled[current], y_train, epochs=25, validation_data=(x_val_scaled[current], y_val), shuffle=False)
+print(clf.predict(x_test_scaled[current]))
 model = clf.export_model()
-
-model.summary()
-
-print(clf.evaluate(x_val_scaled[current], y_val))
+print(model.summary())
